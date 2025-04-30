5c5b8a188ea985050e5858a42a1e99e8115ce069
igaserx <gasseralaa11@gmail.com>
2025-04-30 20:16:34 +0300

    Api Handling

---
+++ lib/core/databases/api/end_points.dart
@@ -0,0 +1,14 @@
+class EndPoints {
+  static const String baserUrl = "";
+  static const String user = "";
+  
+}
+
+class ApiKey {
+  static String id = "id";
+  static String name = "name";
+  static String username = "username";
+  static String email = "email";
+
+
+}
