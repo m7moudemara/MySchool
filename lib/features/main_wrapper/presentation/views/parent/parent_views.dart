ecf5a09bf4f294852940691703d2b6d395269357
igaserx <gasseralaa11@gmail.com>
2025-05-03 11:28:19 +0300

    chat feature

--- lib/features/main_wrapper/presentation/views/parent/parent_views.dart
+++ lib/features/main_wrapper/presentation/views/parent/parent_views.dart
@@ -1,7 +1,7 @@
 import 'package:MySchool/features/dashbord/presentation/views/parent_dashboard_view.dart';
 import 'package:flutter/material.dart';
 
-import '../../../../chat/presentation/views/parent_messages_view.dart';
+import '../../../../school/presentation/views/parent/parent_messages_view.dart';
 import '../../../../school/presentation/views/parent/parent_group_chat_view.dart';
 import '../../../../school/presentation/views/parent/parent_profile_view.dart';
 
