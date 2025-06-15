49c8b42cbd13e9563d47443bee530a639dbfb9fa
alaasabryhammad <alaa.handaza@gmail.com>
2025-06-16 01:24:56 +0300

    feat: Enhance AdminView to fetch and display total counts for fees, parents, and students

--- lib/features/admin/presentation/views/admin_view.dart
+++ lib/features/admin/presentation/views/admin_view.dart
@@ -1,7 +1,11 @@
+import 'dart:convert';
+
 import 'package:MySchool/core/constants/constants.dart';
+import 'package:MySchool/core/constants/strings.dart';
 import 'package:MySchool/core/utils/utils.dart';
 import 'package:MySchool/features/auth/presentation/cubit/user_cubit.dart';
 import 'package:MySchool/features/school/data/models/admin_model.dart';
+import 'package:MySchool/main.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import '../../data/models/compo.dart';
@@ -14,6 +18,7 @@ import 'students/add_students_view.dart';
 import 'subjects/add_subjects_view.dart';
 import 'teacher/add_teachers_view.dart';
 import 'timetable/add_timetables_view.dart';
+import 'package:http/http.dart' as http;
 
 class AdminView extends StatefulWidget {
   static String id = '/AdminView';
@@ -25,22 +30,56 @@ class AdminView extends StatefulWidget {
 
 class _AdminViewState extends State<AdminView> {
   // int totalClasses = 0;
-  late int total_students;
+  // late int total_students;
   late int total_classes;
   late int total_teachers;
   late int total_subjects;
   late int total_timetables;
+  int? total_fees;
+  int? total_parents;
+  int? total_students;
   @override
   void initState() {
     super.initState();
-    final user = context.read<UserCubit>();
-    final userDash = context.read<DashboardUserCubit>().state as AdminDashboardUser;
+    getAlls();
+    // final user = context.read<UserCubit>();
+    final userDash =
+        context.read<DashboardUserCubit>().state as AdminDashboardUser;
     setState(() {
-      total_students = userDash!.total_students;
+      // total_students = userDash.total_students;
       total_classes = userDash.total_classes;
       total_teachers = userDash.total_teachers;
-      total_subjects = userDash.total_classes;
-      total_timetables = userDash.total_classes;
+      total_subjects = userDash.total_subjects;
+      total_timetables = userDash.total_timetables;
+    });
+  }
+
+  getAlls() async {
+    Map<String, String> headersx = {
+      'Authorization': 'Bearer ${await sharedPrefController.getToken()}',
+      'Accept': 'application/json',
+    };
+    final response = await http.get(
+      Uri.parse('$baseUrl/api/fees?PageSize=500'),
+      headers: headersx,
+    );
+    List fees = jsonDecode(response.body)['data'];
+
+    final response1 = await http.get(
+      Uri.parse('$baseUrl/api/accounts?PageSize=500&Where%5Brole%5D=Guardian'),
+      headers: headersx,
+    );
+    List parents = jsonDecode(response1.body)['data'];
+
+    final response2 = await http.get(
+      Uri.parse('$baseUrl/api/accounts?PageSize=500&Where%5Brole%5D=Student'),
+      headers: headersx,
+    );
+    List students = jsonDecode(response2.body)['data'];
+    setState(() {
+      total_fees = fees.length;
+      total_parents = parents.length;
+      total_students = students.length;
     });
   }
 
@@ -56,43 +95,43 @@ class _AdminViewState extends State<AdminView> {
         route: AddClassView.id,
       ),
       Compo(
-        count: 50,
+        count: total_subjects,
         image: "assets/subjectsadmin.png",
         title: "Subjects",
         route: AddSubjectView.id,
       ),
       Compo(
-        count: 50,
+        count: total_teachers,
         image: "assets/teachersadmin.png",
         title: "Teachers",
         route: AddTeachersView.id,
       ),
       Compo(
-        count: 50,
+        count: total_parents ?? 0,
         image: "assets/parentsadmin.png",
         title: "Parents",
         route: AddParentsView.id,
       ),
       Compo(
-        count: total_students,
+        count: total_students ?? 0,
         image: "assets/studentsadmin.png",
         title: "Students",
         route: AddStudentsView.id,
       ),
       Compo(
-        count: 50,
+        count: total_timetables,
         image: "assets/timetablesadmin.png",
         title: "TimeTables",
         route: AddTimeTableView.id,
       ),
       Compo(
-        count: 0,
+        count: total_fees ?? 0,
         image: "assets/feesadmin.png",
         title: "Fees",
         route: AddFeesView.id,
       ),
       Compo(
-        count: 0,
+        count: 72,
         image: "assets/resultsadmin.png",
         title: "Results",
         route: AddResultsView.id,
